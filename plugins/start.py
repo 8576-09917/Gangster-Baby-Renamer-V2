eaab5d425667c4f3120f20ec1952fff8425d1751
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-12-21 17:19:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,8 +17,8 @@ import os
 
 CHANNEL = os.environ.get('CHANNEL', "")
 STRING = os.environ.get("STRING", "")
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
-bot_username = os.environ.get("BOT_USERNAME","GangsterBaby_renamer_BOT")
+ADMIN = int(os.environ.get("ADMIN", 1947565279))
+bot_username = os.environ.get("BOT_USERNAME","Rename_fastCbbot")
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 token = os.environ.get('TOKEN', '')
 botid = token.split(':')[0]
